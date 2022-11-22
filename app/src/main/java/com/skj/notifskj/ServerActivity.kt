40c983435cc31a85326a6a0c3270635a73d9c6dc
feliseureka <feliseureka@gmail.com>
2022-11-22 12:52:25 +0700

    updated Server Activity

--- app/src/main/java/com/skj/notifskj/ServerActivity.kt
+++ app/src/main/java/com/skj/notifskj/ServerActivity.kt
@@ -1,6 +1,11 @@
 package com.skj.notifskj
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.LinkProperties
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.text.format.Formatter
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +17,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.skj.notifskj.ui.theme.NotifSKJTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 
 class ServerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,7 +50,7 @@ class ServerActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateEditText(canBeEdited: Boolean, value: String){
+fun CreateEditText(label: String){
     var text by rememberSaveable { mutableStateOf("Text") }
 
     TextField(
@@ -51,7 +58,7 @@ fun CreateEditText(canBeEdited: Boolean, value: String){
         onValueChange = {
             text = it
         },
-        label = { Text("Label") }
+        label = { Text(label) }
     )
 }
 
@@ -61,17 +68,18 @@ fun PageMainLayout2() {
         Text(text = "You are a server")
         Text(text = "Make sure you're connected in the same Local Network as the clients")
         Text(text = "Send this code to your clients")
-        //Get ip and change to other e.g. base64
-        //Log of clients
-        CreateEditText(canBeEdited = true, value = "Hello")
-        Text(text = "Notification Description")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    NotifSKJTheme {
-        PageMainLayout2()
+        val connectivityManager = LocalContext.current.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var link = connectivityManager.getLinkProperties(connectivityManager.activeNetwork) as LinkProperties
+        val ip = link.linkAddresses[0].address.hostAddress
+        if (ip != null) {
+            Text("Code : $ip")
+        }
+        Text("Clients Logged : ")
+        Text("Send Notification")
+        CreateEditText(label = "Title")
+        CreateEditText(label = "Description")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Send Notification")
+        }
     }
 }
