babb1f25ce47db8d11ae4ce6b9754b6a4b1021ea
feliseureka <feliseureka@gmail.com>
2022-11-22 13:40:17 +0700

    Added Notification Service

--- app/src/main/java/com/skj/notifskj/ClientActivity.kt
+++ app/src/main/java/com/skj/notifskj/ClientActivity.kt
@@ -1,5 +1,11 @@
 package com.skj.notifskj
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +20,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.skj.notifskj.ui.theme.NotifSKJTheme
 
 class ClientActivity : ComponentActivity() {
@@ -30,6 +38,40 @@ class ClientActivity : ComponentActivity() {
                 }
             }
         }
+        createNotificationChannel()
+        //sendNotification()
+    }
+
+    fun createNotification(title: String, desc: String){
+        createNotificationChannel()
+        sendNotification(title, desc)
+    }
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val desc = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("01", name, importance).apply {
+                description = desc
+            }
+            val notificationManager:NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(title: String, desc: String){
+        val intent = Intent(this, ClientActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val builder = NotificationCompat.Builder(this, "01").setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(desc)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+        with(NotificationManagerCompat.from(this)){
+            notify(101, builder.build())
+        }
     }
 }
 
@@ -43,6 +85,7 @@ fun PageMainLayout3() {
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Start Communicating")
         }
+        Text(text = "You're not connected to any Server!")
     }
 }
 
